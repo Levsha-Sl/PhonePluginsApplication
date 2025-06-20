e25c43386e3a24b9240cc167df7ee712c56e97c7
Vyacheslav Shirnin <themindsaund@gmail.com>
2025-06-20 16:34:53 +0700

    Плагин для сохранения пользователей в JSON файл

---
+++ EmployessUploaderPlugin/Plugin.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using PhoneApp.Domain.Attributes;
+using PhoneApp.Domain.DTO;
+using PhoneApp.Domain.Interfaces;
+
+namespace EmployessUploaderPlugin
+{
+  [Author(Name = "Shirnin Vyacheslav")]
+  public class Plugin : IPluggable
+  {
+    private static readonly NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
+    public IEnumerable<DataTransferObject> Run(IEnumerable<DataTransferObject> args)
+    {
+      logger.Info("Uploading employees");
+
+      var employeesList = args.Cast<EmployeesDTO>().ToList();
+
+      try
+      {
+        var employeesJson = JsonConvert.SerializeObject(employeesList, Formatting.Indented);
+        File.WriteAllText(Path.Combine(Directory.GetCurrentDirectory(), "Employees.json"), employeesJson);
+        logger.Info($"List employees saved");
+      }
+      catch (Exception ex)
+      {
+        logger.Error($"List employees not saved: {ex.Message}");
+      }
+
+      return employeesList.Cast<DataTransferObject>();
+    }
+  }
+}
---
+++ EmployessUploaderPlugin/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные со сборкой.
+[assembly: AssemblyTitle("EmployessUploaderPlugin")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EmployessUploaderPlugin")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("ddd24655-7ec0-4ff9-b10f-d95b165c3edb")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
